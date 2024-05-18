ceafc3d39066f8083c216d4596945688bf30130c
sryang <sarang628@gmail.com>
2024-05-18 18:25:20 +0800

    update

--- app/src/main/java/com/sryang/imagepager/MainActivity.kt
+++ app/src/main/java/com/sryang/imagepager/MainActivity.kt
@@ -1,11 +1,9 @@
 package com.sryang.imagepager
 
-import TorangAsyncImage
 import ZoomableTorangAsyncImage
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -16,7 +14,6 @@ import com.sryang.imagepager.ui.theme.ImagePagerTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             ImagePagerTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
